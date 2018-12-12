f74ee050434d0288ec9d69b9d35cb57eec81a8a8
Amit Khatri <aamita96@gmail.com>
2018-12-12 19:01:06 +0530

    Angular UI Added

---
+++ frontend/src/app/servers.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+
+@Injectable()
+export class FileUploadService{
+    constructor(private http: Http){}
+    fileUpload(UploadedForm){
+        // var headers = new Headers();
+        // headers.append('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        // headers.append("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT,OPTIONS");
+        // headers.append('Access-Control-Allow-Origin', '*');
+        // headers.append('Access-Control-Allow-Credentials', 'true');
+        // console.log("file to upload", JSON.stringify(UploadedForm));
+           return this.http.post('http://localhost:8080/pcb/api/uploadAndExtract',UploadedForm);
+        }
+    
+}
