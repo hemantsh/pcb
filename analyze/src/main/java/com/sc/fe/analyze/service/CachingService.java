c79fa21c919e9848ae5356e61996a767dae95c03
hsharma3 <hsharma4@apple.com>
2019-01-14 01:56:37 -0800

    CacheService

--- analyze/src/main/java/com/sc/fe/analyze/service/CachingService.java
+++ analyze/src/main/java/com/sc/fe/analyze/service/CachingService.java
@@ -3,7 +3,9 @@ package com.sc.fe.analyze.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CachingService {
 
 	@Autowired
