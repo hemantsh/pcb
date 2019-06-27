45b88f1d86a84e3d66191f7c1343658bdc8d041e
Amit Khatri <amit@biz4group.com>
2019-06-27 17:18:59 +0530

    Error CodeMap Junit Test Cases.

--- analyze/src/test/java/com/sc/fe/analyze/util/ErrorCodeMapTest.java
+++ analyze/src/test/java/com/sc/fe/analyze/util/ErrorCodeMapTest.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
  * @author pc
  */
 public class ErrorCodeMapTest {
-    
+
     public ErrorCodeMapTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -43,12 +43,46 @@ public class ErrorCodeMapTest {
     @Test
     public void testGetCodeForFileType() {
         System.out.println("getCodeForFileType");
+
         String fileType = "";
-        ErrorCodes expResult = null;
+        ErrorCodes expResult = ErrorCodes.V0000;
         ErrorCodes result = ErrorCodeMap.getCodeForFileType(fileType);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+
+        fileType = "drill";
+        expResult = ErrorCodes.V0001;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+
+        fileType = "signal";
+        expResult = ErrorCodes.V0002;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+
+        fileType = "power_ground";
+        expResult = ErrorCodes.V0003;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+
+        fileType = "solder_mask";
+        expResult = ErrorCodes.V0004;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+        
+        fileType = "silk_screen";
+        expResult = ErrorCodes.V0005;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+        
+        fileType = "solder_paste";
+        expResult = ErrorCodes.V0006;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
+
+        fileType = "gerber";
+        expResult = ErrorCodes.V0011;
+        result = ErrorCodeMap.getCodeForFileType(fileType);
+        assertEquals(expResult, result);
     }
-    
+
 }
